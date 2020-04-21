924c958283c20bb5f3dafbccdaf3e45081e98035
fargx32mkx <54091479+fargx32mkx@users.noreply.github.com>
2020-04-21 15:56:29 -0500

    Add files via upload

---
+++ lista/main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "list.h"
+
+int main (){
+    list *l;
+    l=create_list();
+    add_init(l , 0, 1);
+    add_end(l , 2,1);
+    print_list(l);
+    serach_data(l,2);
+    delete_init(l,1);
+    delete_data(l);
+    l=NULL;
+    return 0;
+
+}
